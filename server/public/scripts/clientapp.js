695a1281a5ab7c8e797177724379ff7d383d0366
Liz Haakenson <lhaakens3@gmail.com>
2016-08-25 12:07:07 -0500

    lecture changes to remove jQuery and add Angular functionality

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,13 +1,18 @@
-$(document).ready(function() {
-  console.log("Let's get started, yo!");
-  getTasks();
+var myApp = angular.module("myApp", []);  //dependencies injection in the array
 
-  // Event listeners
-  $("#taskForm").on("submit", saveTask);
-  $("#task-container").on("click", ".completeTask", completeTask);
-  $("#task-container").on("click", ".deleteTask", deleteTask);
 
-});
+
+
+// $(document).ready(function() {
+//   console.log("Let's get started, yo!");
+//   getTasks();
+//
+//   // Event listeners
+//   $("#taskForm").on("submit", saveTask);
+//   $("#task-container").on("click", ".completeTask", completeTask);
+//   $("#task-container").on("click", ".deleteTask", deleteTask);
+//
+// });
 
 // Send task object to the server
 function saveTask() {
